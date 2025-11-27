f97d84989b3e1d2bdaeb17513c03f8485fe1c018
Emipano <emilianobracci@tiscali.it>
2025-11-27 18:15:16 +0100

    seconda parte

---
+++ Laser Diodo/Apertura angolare/apertura_angolare.py
@@ -0,0 +1,20 @@
+import numpy as np
+import matplotlib.pyplot as plt 
+
+phi_o, I_o, dI_o = np.loadtxt("/home/emiliano/Documenti/Lab. Materia/Laser Diodo/Apertura angolare/orizzontale_1.txt", unpack=True)
+phi_o-=270
+phi_v, I_v, dI_v = np.loadtxt("/home/emiliano/Documenti/Lab. Materia/Laser Diodo/Apertura angolare/verticale_1.txt", unpack=True)
+phi_v-=270
+
+
+
+fig = plt.figure()
+plt.errorbar(phi_o, I_o, yerr=dI_o, fmt=".-")
+plt.errorbar(phi_v, I_v, yerr=dI_v, fmt=".-")
+plt.legend("Orizzontale", "Verticale")
+plt.grid(which="both", ls="dashed", color="gray")
+plt.title("Apertura Angolare")
+plt.xlabel("\phi[°]")
+plt.ylabel("I [\mu A]")
+plt.show()
+
